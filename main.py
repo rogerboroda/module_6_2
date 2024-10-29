29f4ed9426c821ea409f38752d414b12e51095d0
rogerboroda <roger_boroda@mail.ru>
2024-10-29 18:27:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        self.owner = owner
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
+
+    def get_model(self) -> str:
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self) -> str:
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self) -> str:
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in (color.lower() for color in Vehicle.__COLOR_VARIANTS):
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        super().__init__(owner, model, color, engine_power)
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1.print_info()
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+vehicle1.print_info()
+
+# Вывод на консоль:
+#
+# Модель: Toyota Mark II
+# Мощность двигателя: 500
+# Цвет: blue
+# Владелец: Fedos
+# Нельзя сменить цвет на Pink
+# Модель: Toyota Mark II
+# Мощность двигателя: 500
+# Цвет: BLACK
+# Владелец: Vasyok
